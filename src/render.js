3816892e2c172b8cd660cc0bf5e5abf93a089675
Bairui Su <subairui@icloud.com>
2024-04-28 09:35:57 +0800

    Add label for non-leaf block (#8)

--- src/render.js
+++ src/render.js
@@ -1,5 +1,5 @@
 import { load } from "js-yaml";
-import { create, schemeObservable10, scaleOrdinal, sort, hsl, hierarchy, treemap } from "d3";
+import { create, hierarchy, treemap } from "d3";
 import { treemapFlex } from "d3-treemap-flex";
 
 function isPlainObject(value) {
@@ -9,14 +9,15 @@ function isPlainObject(value) {
 function tree(options) {
   const { root: _root, props } = options;
   const root = hierarchy({ root: _root }, children);
-  root.each(bindData);
+
+  root.each(data).each(flex);
 
   function children(d) {
     if (typeof d === "string") return null;
     return d[Object.keys(d)[0]];
   }
 
-  function bindData(d) {
+  function data(d) {
     const { data } = d;
     const key = isPlainObject(data) ? Object.keys(data)[0] : data;
     const { label = key, visible = true, ...restProps } = props[key] || {};
@@ -24,44 +25,48 @@ function tree(options) {
     Object.assign(d.data, { key, label, visible, ...restProps });
   }
 
+  function flex(d) {
+    if (!Array.isArray(d.data.flex) || !Array.isArray(d.children)) return;
+    for (let i = 0; i < d.children.length; i++) {
+      d.children[i].data.value = d.data.flex[i] ?? 1;
+    }
+  }
+
   return root;
 }
 
 function visible(key, padding) {
-  return (d) => d.data[key] ?? (d.data.visible ? padding : 0);
+  const p = typeof padding === "function" ? padding : () => padding;
+  return (d) => d.data[key] ?? (d.data.visible ? p(d) : 0);
+}
+
+function labelHeight(d) {
+  return 18;
 }
 
 function renderJSON(options) {
   const { layout } = options;
-  const {
-    width,
-    height,
-    padding = 0,
-    paddingInner = padding,
-    paddingOuter = visible("paddingOuter", padding),
-    paddingLeft = visible("paddingLeft", padding),
-    paddingRight = visible("paddingRight", padding),
-    paddingBottom = visible("paddingBottom", padding),
-    paddingTop = visible("paddingTop", padding),
-    round = false,
-  } = layout;
+  const { width, height, padding = 0, paddingTop = padding * 2 + labelHeight() } = layout;
+
+  const labelX = (d) => (d.children ? 0 : (d.x1 - d.x0) / 2);
+  const labelY = (d) => (d.children ? 0 : (d.y1 - d.y0) / 2);
+  const labelTextAnchor = (d) => (d.children ? "start" : "middle");
+  const labelDominantBaseline = (d) => (d.children ? "middle" : "middle");
+  const labelDx = (d) => (d.children ? padding : 0);
+  const labelDy = (d) => (d.children ? paddingTop / 2 : 0);
+  const hasLabel = d => d.data.label && d.children && d.data.key !== "root";
 
   const root = treemap()
     .tile(treemapFlex())
     .size([width, height])
-    .paddingInner(paddingInner)
-    .paddingOuter(paddingOuter)
-    .paddingTop(paddingTop)
-    .paddingRight(paddingRight)
-    .paddingBottom(paddingBottom)
-    .paddingLeft(paddingLeft)
-    .round(round)(tree(options));
+    .paddingInner(padding)
+    .paddingOuter(visible("paddingOuter", padding))
+    .paddingTop(visible("paddingTop", (d) => (hasLabel(d) ? paddingTop : padding)))
+    .paddingRight(visible("paddingRight", padding))
+    .paddingBottom(visible("paddingBottom", padding))
+    .paddingLeft(visible("paddingLeft", padding))(tree(options));
 
   const nodes = root.descendants().filter((d) => d.data.visible);
-
-  const color = scaleOrdinal()
-    .domain(sort(Array.from(new Set(nodes.map((d) => d.height)))))
-    .range(schemeObservable10);
 
   const svg = create("svg").attr("viewBox", `0 0 ${width} ${height}`);
 
@@ -74,15 +79,17 @@ function renderJSON(options) {
   g.append("rect")
     .attr("width", (d) => d.x1 - d.x0)
     .attr("height", (d) => d.y1 - d.y0)
-    .attr("fill", (d) => (d.data.key === "root" ? "#fff" : hsl(color(d.height)).brighter(1.2)))
-    .attr("stroke", (d) => (d.data.key === "root" ? "#fff" : color(d.height)));
+    .attr("fill", "#fff")
+    .attr("stroke", (d) => (d.data.key === "root" ? "none" : "#000"));
 
-  g.filter((d) => !d.children)
+  g.filter((d) => d.data.label && d.data.key !== "root")
     .append("text")
-    .attr("x", (d) => (d.x1 - d.x0) / 2)
-    .attr("y", (d) => (d.y1 - d.y0) / 2)
-    .attr("text-anchor", "middle")
-    .attr("dominant-baseline", "middle")
+    .attr("x", labelX)
+    .attr("y", labelY)
+    .attr("text-anchor", labelTextAnchor)
+    .attr("dominant-baseline", labelDominantBaseline)
+    .attr("dx", labelDx)
+    .attr("dy", labelDy)
     .text((d) => d.data.label);
 
   return svg.node();
