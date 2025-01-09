84a57083bc5ccda66e03ea68fe4c4d443313f893
pearmini <subairui@icloud.com>
2025-01-09 13:03:10 -0500

    Support vertical option

--- packages/diyajs/test/diagrams/index.js
+++ packages/diyajs/test/diagrams/index.js
@@ -1,1 +1,2 @@
 export {simpleBlockDiagram, simpleBlockDiagram1} from "./simple-block-diagram.js";
+export {verticalSimpleBlockDiagram, verticalSimpleBlockDiagram1} from "./vertical-simple-block-diagram.js";
---
+++ packages/diyajs/test/diagrams/vertical-simple-block-diagram.js
@@ -0,0 +1,15 @@
+export const verticalSimpleBlockDiagram = `
+nodes: [a, b, c]
+
+config:
+  nodes:
+    direction: column
+`;
+
+export const verticalSimpleBlockDiagram1 = `
+nodes: [aaaa, b, cc]
+
+config:
+  nodes:
+    direction: column
+`;
