10233f2d261d3122852d2acf1c270133f24526e3
Bairui Su <subairui@icloud.com>
2025-01-09 13:04:20 -0500

    Support vertical option (#20)

--- packages/diyajs/src/auto.js
+++ packages/diyajs/src/auto.js
@@ -8,20 +8,40 @@ export function auto(root, {padding = 5, margin = 10} = {}) {
       d.h = height + padding * 2;
       return;
     }
-    const maxWidth = Math.max(...d.children.map((c) => c.w));
-    const maxHeight = Math.max(...d.children.map((c) => c.h));
 
-    let x = 0;
-    for (const child of d.children) {
-      child.x = x;
-      child.y = 0;
-      child.w = maxWidth;
-      child.h = maxHeight;
-      x += maxWidth + margin;
+    const {direction = "row"} = d.data;
+
+    if (direction === "row") {
+      const maxWidth = Math.max(...d.children.map((c) => c.w));
+      const maxHeight = Math.max(...d.children.map((c) => c.h));
+
+      let x = 0;
+      for (const child of d.children) {
+        child.x = x;
+        child.y = 0;
+        child.w = maxWidth;
+        child.h = maxHeight;
+        x += maxWidth + margin;
+      }
+
+      d.w = x - margin;
+      d.h = maxHeight;
+    } else {
+      const maxWidth = Math.max(...d.children.map((c) => c.w));
+      const maxHeight = Math.max(...d.children.map((c) => c.h));
+
+      let y = 0;
+      for (const child of d.children) {
+        child.x = 0;
+        child.y = y;
+        child.w = maxWidth;
+        child.h = maxHeight;
+        y += maxHeight + margin;
+      }
+
+      d.w = maxWidth;
+      d.h = y - margin;
     }
-
-    d.w = x - margin;
-    d.h = maxHeight;
   });
 
   root.eachBefore((d) => {
