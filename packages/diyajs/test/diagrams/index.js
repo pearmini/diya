f9ea4e15fdb0ff2e47549f83300f903d09677b60
Bairui Su <subairui@icloud.com>
2025-01-09 13:18:16 -0500

    Support wrap option (#21)

--- packages/diyajs/test/diagrams/index.js
+++ packages/diyajs/test/diagrams/index.js
@@ -1,2 +1,3 @@
 export {simpleBlockDiagram, simpleBlockDiagram1} from "./simple-block-diagram.js";
 export {verticalSimpleBlockDiagram, verticalSimpleBlockDiagram1} from "./vertical-simple-block-diagram.js";
+export {wrapSimpleBlockDiagram, wrapSimpleBlockDiagram1} from "./wrap-simple-block-diagram.js";
