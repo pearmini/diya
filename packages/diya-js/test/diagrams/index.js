3d4fff3cb4fbbc3cfbfa26c420bcee87a7bd5809
Bairui Su <subairui@icloud.com>
2025-01-18 23:30:47 -0500

    Remove diagrams (#24)

--- packages/diya-js/test/diagrams/index.js
+++
@@ -1,9 +0,0 @@
-export {simpleBlockDiagram, simpleBlockDiagram1} from "./simple-block-diagram.js";
-export {verticalSimpleBlockDiagram, verticalSimpleBlockDiagram1} from "./vertical-simple-block-diagram.js";
-export {wrapSimpleBlockDiagram, wrapSimpleBlockDiagram1} from "./wrap-simple-block-diagram.js";
-export {
-  flexSimpleBlockDiagram,
-  flexSimpleBlockDiagram1,
-  flexSimpleBlockDiagram2,
-  flexSimpleBlockDiagram3,
-} from "./flex-simple-block-diagram.js";
--- packages/diya-js/test/diagrams/simple-block-diagram.js
+++
@@ -1,7 +0,0 @@
-export const simpleBlockDiagram = `
-nodes: [a, b, c]
-`;
-
-export const simpleBlockDiagram1 = `
-nodes: [aaaa, b, cc]
-`;
